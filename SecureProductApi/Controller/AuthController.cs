dc8b93fb82f134308c7ce6be8e33ea04c7b1a992
Abhay Rathod <abhayrathod0204@gmail.com>
2026-01-20 10:32:48 +0530

    20-01-2026

---
+++ SecureProductApi/Controller/AuthController.cs
@@ -0,0 +1,44 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+[ApiController]
+[Route("api/auth")]
+
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _config;
+    public AuthController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+
+    [HttpPost("login")]
+    public IActionResult Login()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, "student"),
+            new Claim(ClaimTypes.Role,"Admin")
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_config["JwtKey"]));
+
+        var token = new JwtSecurityToken(
+            issuer: "SecureApi",
+            audience: "SecureApiUser",
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(30),
+            signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
+
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token)
+
+        });
+    }
+}
